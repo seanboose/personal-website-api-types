36762b52a669a340101933423464c23bba672b38
sean boose <seanboose@proton.me>
2025-11-04 14:33:57 -0700

    change auth to access for some vars, add more errors

--- dist/auth.d.ts
+++ dist/auth.d.ts
@@ -1,11 +1,13 @@
 export declare const authRequestHeaderName = "internal-auth-key";
+export declare const authRequestClientKey = "auth-request-client";
 export declare const authAccessTokenName = "access_token";
 export declare const authRefreshTokenName = "refresh_token";
-export declare const authRequestClientKey = "auth-request-client";
 export declare const authErrors: {
-    authTokenNotProvided: string;
-    authTokenExpired: string;
-    authTokenInvalid: string;
+    accessTokenExpired: string;
+    accessTokenInvalid: string;
+    accessTokenNotProvided: string;
+    authKeyInvalid: string;
+    clientNotProvided: string;
     refreshTokenExpired: string;
     refreshTokenInvalid: string;
 };
--- src/auth.ts
+++ src/auth.ts
@@ -1,21 +1,23 @@
 // header for initial auth requests, must be paired with auth secret
 export const authRequestHeaderName = "internal-auth-key";
 
+// must be passed with a value in body of auth requests
+export const authRequestClientKey = "auth-request-client";
+
 // name of (short term) access token returned by auth requests
 export const authAccessTokenName = "access_token";
 
 // name of (long term) refresh token returned by auth requests
 export const authRefreshTokenName = "refresh_token";
 
-// must be passed with a value in body of auth requests
-export const authRequestClientKey = "auth-request-client";
-
 export const authErrors = {
-  authTokenNotProvided: "TOKEN_NOT_PROVIDED",
-  authTokenExpired: "AUTH_TOKEN_EXPIRED",
-  authTokenInvalid: "AUTH_TOKEN_INVALID",
+  accessTokenExpired: "ACCESS_TOKEN_EXPIRED",
+  accessTokenInvalid: "ACCESS_TOKEN_INVALID",
+  accessTokenNotProvided: "ACCESS_TOKEN_NOT_PROVIDED",
+  authKeyInvalid: "AUTH_KEY_INVALID",
+  clientNotProvided: "CLIENT_NOT_PROVIDED",
   refreshTokenExpired: "REFRESH_TOKEN_EXPIRED",
   refreshTokenInvalid: "REFRESH_TOKEN_INVALID"
 };
 
-// TODO merge api-types changes into master once done
+// TODO merge api-types changes into master and publish minor once done
